89e97dd77685c324a409ec64a5ff690483ef55e9
Abhit0782 <abhishekt0782@gmail.com>
2026-01-27 13:46:24 +0530

    Update service worker cache configuration and assets

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-const CACHE_NAME = 'smart-strip-v1';
-const urlsToCache = [
-  './',
+const CACHE_NAME = 'tathya-v1';
+const ASSETS = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/3063/3063176.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
